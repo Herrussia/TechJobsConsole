bc6043f10b106ff56e41498a151dae75e9774e01
Amelia <ameliasaylor@gmail.com>
2021-12-16 13:35:04 -0600

    fixed search term uppercase issue

--- src/TechJobsConsole/Program.cs
+++ src/TechJobsConsole/Program.cs
@@ -57,14 +57,14 @@ namespace TechJobsConsole
                     // What is their search term?
                     Console.WriteLine("\nSearch term: ");
                     string searchTerm = Console.ReadLine();
-
+                    //searchTerm = searchTerm.ToLower();
                     List<Dictionary<string, string>> searchResults;
 
                     // Fetch results
                     if (columnChoice.Equals("all"))
                     {
                         //Console.WriteLine("Search all fields not yet implemented.");
-                        PrintJobs(JobData.FindByValue(searchTerm));
+                        PrintJobs(JobData.FindByValue(searchTerm.ToLower()));
                     }
                     else
                     {
